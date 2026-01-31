2dc45fce48cba9c09b6855bb55c86aa3fbd1bb3f
william <espiw@farmingdale.edu>
2026-01-31 11:51:16 -0500

    declared our constants

--- my-react-app-to-do-list/src/ToDoList.jsx
+++ my-react-app-to-do-list/src/ToDoList.jsx
@@ -1,6 +1,14 @@
 import React, {useState} from "react";
 
 function ToDoList() {
+    // tasks will be an array of strings with a setter function
+    // initial state of tasks will be an empty array
+    const [tasks, setTasks] = useState([]);
+    // new tasks will be an array of strings with a setter function
+    // initial state of new tasks will be an empty string
+    // this will handle each individual new task added
+    const [newTask, setNewTasks] = useState("");
+
     return(
         <p>hello</p>
     );
